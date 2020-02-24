a9b452cc26ea1251cf3f2fa14b04aa7594dbf847
github username <github email address>
2020-02-24 14:38:47 -0800

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
   
@@ -6,6 +7,25 @@ class StudentsController < ApplicationController
   end
 
   def show
+    @student = Student.find_by_id(params[:id])
+    if @student.active == "t"
+      @status = 'active' 
+    elsif @student.active == "f" 
+      @status = 'inactive'
+    end
+  end
+
+  def activate
+    @student = Student.find(params[:id])
+
+    if @student.active
+       @status = false
+     else
+       @status = true
+    end
+    #@student.active = @status
+    @student.update(params[:id], active: @status)
+    redirect_to student_path(@student)
   end
 
   private
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,13 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def active 
+    if @active == 't'
+      status = true 
+    elsif @active = 'f'
+      status = false 
+    end
+    status
+  end
 end
